44239d3749b7b345ab7616c0585461f5e5f26f63
evgenii.mishalov@sap.com <evgenii.mishalov@sap.com>
2024-12-16 21:26:47 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,17 @@ repositories {
     mavenCentral()
 }
 
+val extraLibs by configurations.creating
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.json:json:20240303")
     implementation("org.postgresql:postgresql:42.3.0")
     implementation("commons-codec:commons-codec:1.17.1")
+
+    extraLibs("org.postgresql:postgresql:42.3.0")
+    configurations.compileClasspath.get().extendsFrom(extraLibs)
 }
 
 tasks.test {
@@ -29,4 +34,8 @@ tasks.jar {
             "Main-Class" to "org.example.Main" // Replace with your main class
         )
     }
+    archiveBaseName.set("${project.name}-all")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
 }
