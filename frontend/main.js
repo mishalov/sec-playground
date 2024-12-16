3dae541be2a974f1b2736ddbc3de7326d9a736e3
evgenii.mishalov@sap.com <evgenii.mishalov@sap.com>
2024-12-16 23:33:00 +0100

    Update

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,7 @@ import { saveToDocument } from './script/utils/globalStorage';
 import './style.css'
 
 // if localhost then localhost:8080 else vps-e1b58ab7.vps.ovh.net/api
-const backendUrl = window.location.hostname === "localhost" ? 'http://localhost:8080/api' : 'https://vps-e1b58ab7.vps.ovh.net/api';
+const backendUrl = window.location.hostname === "localhost" ? 'http://localhost:8080/api' : 'http://vps-e1b58ab7.vps.ovh.net/api';
 const api = new API(backendUrl);
 const auth = new Authentication(api);
 api.setAuthentication(auth);
